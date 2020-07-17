7fce4a9a8f9e150bd1728e79e38654c2c57411cb
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-07-17 18:07:30 +0000

    README file

--- math/0x00-linear_algebra/100-slice_like_a_ninja.py
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -4,27 +4,11 @@
 
 def np_slice(matrix, axes={}):
     """ slices a matrix along a specific axis """
-    if axes == {}:
-        return matrix
-    tuplas = {
-        0: (None, None, None),
-        1: (None, None, None),
-        2: (None, None, None),
-        3: (None, None, None),
-        4: (None, None, None)
-    }
+    tuplas = {}
+    highest = max(axes.keys())
+    for i in range(highest):
+        tuplas[i] = (None, None, None)
     for key in axes.keys():
         tuplas[key] = axes[key]
-    if matrix.ndim == 1:
-        return matrix[slice(*tuplas[0])]
-    if matrix.ndim == 2:
-        return matrix[slice(*tuplas[0]), slice(*tuplas[1])]
-    if matrix.ndim == 3:
-        return matrix[slice(*tuplas[0]), slice(*tuplas[1]), slice(*tuplas[2])]
-    if matrix.ndim == 4:
-        return matrix[slice(*tuplas[0]), slice(*tuplas[1]),
-                      slice(*tuplas[2]), slice(*tuplas[3])]
-    if matrix.ndim == 5:
-        return matrix[slice(*tuplas[0]), slice(*tuplas[1]),
-                      slice(*tuplas[2]), slice(*tuplas[3]),
-                      tuplas[4]]
+
+    return matrix[tuple(slice(*i) for i in tuplas.values())]
