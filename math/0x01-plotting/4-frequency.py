458cae08aa3ce49d642cf4b909dfd816a9f4018e
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-07-20 17:08:14 +0000

    histogram of student scores for a project

--- math/0x01-plotting/4-frequency.py
+++ math/0x01-plotting/4-frequency.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 np.random.seed(5)
 student_grades = np.random.normal(68, 15, 50)
 
+#plot code
 fig, ax = plt.subplots()
 ax.hist(student_grades, bins=[40, 50, 60, 70, 80, 90, 100], edgecolor ='k')
 plt.xlim(0, 100)
