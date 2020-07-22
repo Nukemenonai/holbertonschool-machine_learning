0ad9a9f28cb4291e89d920ff7c3b9776716dfcc4
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-07-22 22:50:23 +0000

    function that calculates the derivative of a polinomial based on position

---
+++ math/0x02-calculus/10-matisse.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" this module contains the poly_derivative function"""
+
+
+def poly_derivative(poly):
+    """ calculates the derivative of a polinomial """
+    deriv = [0]
+    for i in range(len(poly)):
+        if i == 0:
+            continue
+        else:
+            deriv.append(poly[i] * (i))
+    if deriv[0] == 0 and len(deriv) != 1:
+        del deriv[0]
+    return deriv
