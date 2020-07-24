7ac968b6af0390d9ec710179b7fdefeedfcd0d3e
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-07-24 19:49:37 +0000

    fix: pycodestyle

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -9,4 +9,4 @@ def summation_i_squared(n):
     elif n <= 0:
         return None
     else:
-        return int((n * (n + 1) * ((2*n) +1)) / 6)
+        return int((n * (n + 1) * ((2 * n) + 1)) / 6)
