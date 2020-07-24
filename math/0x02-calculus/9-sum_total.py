80921b145f2d5fc2adfd65da6829be3c19b579eb
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-07-24 19:29:43 +0000

    fix: 0 or negative numbers

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -6,5 +6,7 @@ def summation_i_squared(n):
     """ this function calculates the sum notation from i=1 to n """
     if type(n) != int:
         return None
+    elif n <= 0:
+        return None
     else:
         return int((n * (n + 1) * ((2*n) +1)) / 6)
