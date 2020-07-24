555ecd958200cf92b2f33bbc0bdac8d13a58d336
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-07-24 19:16:46 +0000

    cuadratic sum formula applied

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -6,7 +6,5 @@ def summation_i_squared(n):
     """ this function calculates the sum notation from i=1 to n """
     if type(n) != int:
         return None
-    if n == 1:
-        return n
     else:
-        return (n **2 ) + summation_i_squared(n-1)
+        return int((n * (n + 1) * ((2*n) +1)) / 6)
