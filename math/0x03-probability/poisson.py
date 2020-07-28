c1289921d586ba37e7c75cc8e618f4e849251db9
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-07-28 17:25:05 +0000

    updated poisson with method to get the PMF of a given number of successes

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -2,6 +2,9 @@
 """ This module contains the Poisson class"""
 
 
+e = 2.7182818285
+
+
 class Poisson:
     """ this class representas a Poisson distribution"""
     def __init__(self, data=None, lambtha=1.):
@@ -16,4 +19,20 @@ class Poisson:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
+            self.data = data
             self.lambtha = sum(data) / len(data)
+
+    def pmf(self, k):
+        """instance method.
+        calculates the PMF value for a number of successes
+        k represents the number of successes """
+        if type(k) != int:
+            int(k)
+        if k < 0:
+            return 0
+
+        def factorial(n):
+            """returns the factorial of given number """
+            return 1 if (n == 1 or n == 0) else n * factorial(n - 1)
+
+        return ((self.lambtha ** k)*(e ** -(self.lambtha)))/factorial(k)
