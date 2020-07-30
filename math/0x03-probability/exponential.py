0500636588553d6dfeba784e0609c407452e83de
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-07-30 02:52:58 +0000

    update: excluded 0 from positives

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -10,7 +10,7 @@ class Exponential:
     def __init__(self, data=None, lambtha=1.):
         """ class constructor """
         if data is None:
-            if lambtha < 0:
+            if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             else:
                 self.lambtha = float(lambtha)
