9b593ce879d04e5c374932336968521296860b08
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-01 20:27:43 +0000

    update: privatized version of the neuron class

---
+++ supervised_learning/0x00-binary_classification/1-neuron.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""This file contains the Privatized class Neuron """
+import numpy as np
+
+class Neuron:
+    """defines a single neuron performing binary clasification"""
+    def __init__(self, nx):
+        """class constructor """
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """getter for the weight attribute """
+        return self.__W
+
+    @property
+    def b(self):
+        """getter for the bias attribute """
+        return self.__b
+
+    @property
+    def A(self):
+        """getter for the Activated output attribute """
+        return self.__A
