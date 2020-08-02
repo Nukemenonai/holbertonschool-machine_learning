3de2ca0b1e58f1133784ebb8c6b019f11d557e28
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-02 00:22:55 +0000

    update: calculates forward propagation of the neuron

---
+++ supervised_learning/0x00-binary_classification/2-neuron.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""This file contains the Privatized class Neuron """
+import numpy as np
+
+
+class Neuron:
+    """defines a single neuron performing binary clasification"""
+    def __init__(self, nx):
+        """class constructor """
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+        self.nx = nx
+
+    @property
+    def W(self):
+        """getter for the weight attribute """
+        return self.__W
+
+    @property
+    def b(self):
+        """getter for the bias attribute """
+        return self.__b
+
+    @property
+    def A(self):
+        """getter for the Activated output attribute """
+        return self.__A
+
+    def forward_prop(self, X):
+        """ calculates the forward propagation of the neuron"""
+        def sigmoid(a):
+            """ returns the sigmoid activation """
+            return 1/(1 + np.exp(-a))
+        Z = np.matmul(self.__W, X) + self.__b
+        self.__A = sigmoid(Z)
+        return self.__A
