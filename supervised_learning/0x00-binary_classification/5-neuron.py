d7d4155eb97edef32a68f637b8052bdcf96e0833
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-04 03:57:19 +0000

    progress on task 5

---
+++ supervised_learning/0x00-binary_classification/5-neuron.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+"""This file contains the Privatized class Neuron """
+import numpy as np
+
+
+def sigmoid(a):
+    """ returns the sigmoid activation """
+    return 1/(1 + np.exp(-a))
+
+
+class Neuron:
+    """defines a single neuron performing binary clasification"""
+    def __init__(self, nx):
+        """class constructor """
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+        self.nx = nx
+
+    @property
+    def W(self):
+        """getter for the weight attribute """
+        return self.__W
+
+    @property
+    def b(self):
+        """getter for the bias attribute """
+        return self.__b
+
+    @property
+    def A(self):
+        """getter for the Activated output attribute """
+        return self.__A
+
+    def forward_prop(self, X):
+        """ calculates the forward propagation of the neuron"""
+        Z = np.matmul(self.__W, X) + self.__b
+        self.__A = sigmoid(Z)
+        return self.__A
+
+    def cost(self, Y, A):
+        """ calculates the cost of the model using logistic regression"""
+        m = np.shape(Y)
+        return -(1 / m[1]) * np.sum(Y * np.log(A) +
+                                 (1 - Y) * np.log(1.0000001 - A))
+
+    def evaluate(self, X, Y):
+        """ Evaluates the neurons predictions
+        X: contains the input data
+        Y: np.nadarray(1, m) contains the correct labels"""
+        Z = self.forward_prop(X)
+        return np.rint(Z), self.cost(Y, Z)
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """ calculates one pass of the gradient descent on the neuron
+        X: nd array (nx, m) contains input data
+        Y: nd array (1, m) contains correct lables
+        A: nd array (1, m) contains activated output
+        alpha: learning rate
+        """
+        XT = X.transpose()
+        YT = Y.transpose()
+        m = np.shape(Y)
+        loss = A - Y
+        gradient = np.dot(YT, loss) / m[1]
+        self.__W = self.__W - alpha * gradient
+        db = np.dot(Y, loss) / m[1]
+        return self.__W, self.__b
