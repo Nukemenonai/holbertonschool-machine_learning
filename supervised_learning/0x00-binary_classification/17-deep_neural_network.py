5642a580675e8edd0d29a7589b24998c772cedd6
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-07 00:56:11 +0000

    privatized atributes

---
+++ supervised_learning/0x00-binary_classification/17-deep_neural_network.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""this module contains the a deep neural network class"""
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """defines a deep neural network"""
+    def __init__(self, nx, layers):
+        """class constructor"""
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(layers) != list or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        for i in range(self.L):
+            if type(layers[i]) != int or layers[i] < 1:
+                raise TypeError("layers must be a list of positive integers")
+            self.weights['W' + str(i + 1)] = (np.random.randn(layers[i], nx) *
+                                              np.sqrt(2./nx))
+            nx = layers[i]
+            self.weights['b' + str(i + 1)] = np.zeros((layers[i], 1))
+
+    @property
+    def L():
+        """ returns the L privatized property"""
+        return self.__L
+
+    @property
+    def cache():
+        """return the privatized cache property """
+        return self.__cache
+
+    @property
+    def weights():
+        """returns the privatized weight property """
+        return self.__weights
