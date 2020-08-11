d124410874eb4372500f045375e634ff44a7e578
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-11 17:31:00 +0000

    added additional condition

--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -7,5 +7,7 @@ def one_hot_decode(one_hot):
     """ converts a one-hot matrix to a vector of labels
     one-hot: one-hot encoded numpy array
     """
+    if type(one_hot) != np.ndarray:
+        return None
     decode = np.argmax(one_hot, axis=0)
     return decode
