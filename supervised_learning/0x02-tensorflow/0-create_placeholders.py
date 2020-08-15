842ad02492a2da7574fd3ffb9a21aa8c325a0ad8
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-15 19:19:25 +0000

    creates placeholders for the neural network

--- supervised_learning/0x02-tensorflow/0-create_placeholders.py
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -6,6 +6,6 @@ import tensorflow as tf
 
 def create_placeholders(nx, classes):
     """returns two placeholders"""
-    x = tf.placeholder(tf.float32, [None, nx], 'x') 
+    x = tf.placeholder(tf.float32, [None, nx], 'x')
     y = tf.placeholder(tf.float32, [None, classes], 'y')
     return x, y
