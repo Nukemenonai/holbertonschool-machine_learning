69cec8d0b3672eb84798e74a54d859abb0c907a8
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-15 16:59:24 +0000

    fix: axis scope

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -6,5 +6,5 @@ import tensorflow as tf
 
 def calculate_accuracy(y, y_pred):
     """ calculates the accuracy of a prediction """
-    acc = tf.equal(tf.argmax(y), tf.argmax(y_pred))
+    acc = tf.equal(tf.argmax(y, axis=1), tf.argmax(y_pred, axis=1))
     return tf.reduce_mean(tf.cast(acc, tf.float32))
