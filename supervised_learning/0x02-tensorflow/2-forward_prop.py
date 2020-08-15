bd573cbb2bc3197d2ee5dd4b001ff357c7a1c4e5
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-15 21:11:08 +0000

    fix: pycodestyle

--- supervised_learning/0x02-tensorflow/2-forward_prop.py
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -7,7 +7,7 @@ create_layer = __import__('1-create_layer').create_layer
 
 
 def forward_prop(x, layer_sizes=[], activations=[]):
-    """ """
+    """creates forward propagation :v """
     A = create_layer(x, layer_sizes[0], activations[0])
     for i in range(1, len(layer_sizes)):
         A = create_layer(A, layer_sizes[i], activations[i])
