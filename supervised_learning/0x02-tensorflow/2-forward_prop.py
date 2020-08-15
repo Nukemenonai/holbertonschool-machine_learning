e534402df1aaa153a972d9b2ceff0b2f10af7fba
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-15 20:54:13 +0000

    forward propagation with tensorflow

--- supervised_learning/0x02-tensorflow/2-forward_prop.py
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" """
+"""creates the forward propagation graph for the NN"""
 
 
 import tensorflow as tf
@@ -7,7 +7,8 @@ create_layer = __import__('1-create_layer').create_layer
 
 
 def forward_prop(x, layer_sizes=[], activations=[]):
-    """ """
-    for i in range(len(layer_sizes)):
-        A = create_layer(x, layer_sizes[i], activations[i])
-    return A
+        """ """
+        A = create_layer(x, layer_sizes[0], activations[0])
+        for i in range(1, len(layer_sizes)):
+            Y = create_layer(A, layer_sizes[i], activations[i])
+        return Y
