b4babedc82709dd947c32441eb8e291fa9eb0d34
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-15 21:00:07 +0000

    fix: keeping the same variable for all activations

--- supervised_learning/0x02-tensorflow/2-forward_prop.py
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -10,5 +10,5 @@ def forward_prop(x, layer_sizes=[], activations=[]):
         """ """
         A = create_layer(x, layer_sizes[0], activations[0])
         for i in range(1, len(layer_sizes)):
-            Y = create_layer(A, layer_sizes[i], activations[i])
-        return Y
+            A = create_layer(A, layer_sizes[i], activations[i])
+        return A
