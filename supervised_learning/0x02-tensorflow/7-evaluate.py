5379726e1a8d76a3c51b2cdcda208e8a42285c03
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-16 04:12:25 +0000

    loads and evaluates the NN

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -9,15 +9,14 @@ import tensorflow as tf
 
 def evaluate(X, Y, save_path):
     """ evaluates the output of the NN"""
-    saver = tf.train.import_meta_graph(save_path + '.meta')
     with tf.Session as sess:
+        saver = tf.train.import_meta_graph(save_path + '.meta')
         saver.restore(sess, save_path)
         x = tf.get_collection('x')[0]
         y = tf.get_collection('y')[0]
         y_pred = tf.get_collection("y_pred")[0]
         loss = tf.get_collection("loss")[0]
         accuracy = tf.get_collection("accuracy")[0]
-
         output = ses.run(y_pred, feed_dict={x: X, y: Y})
         accuracy = ses.run(accuracy, feed_dict={x: X, y: Y})
         cost = ses.run(loss, feed_dict={x: X, y: Y})
