814fd8b11b544f545e4b7560de27b802cad6e79e
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-20 22:53:03 +0000

    trains a loaded model using mini batch gradient descent

---
+++ supervised_learning/0x03-optimization/3-mini_batch.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+""" contains the train_mini_batch module"""
+
+import numpy as np
+import tensorflow as tf
+shuffle_data = __import__('2-shuffle_data').shuffle_data
+
+
+def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
+                     epochs=5, load_path="/tmp/model.ckpt",
+                     save_path="/tmp/model.ckpt"):
+    """ trains a neural network using mini batch gradient descent"""
+
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph(load_path + '.meta')
+        saver.restore(sess, load_path)
+        x = tf.get_collection("x")[0]
+        y = tf.get_collection("y")[0]
+        loss = tf.get_collection("loss")[0]
+        accuracy = tf.get_collection("accuracy")[0]
+        train_op = tf.get_collection("train_op")[0]
+
+        mbit = X_train.shape[0] / batch_size
+        mbit = int(mbit + 1) if type(mbit) != int else int(bit)
+
+        for i in range(epochs + 1):
+            train_cost = sess.run(loss, feed_dict={x: X_train, y: Y_train})
+            train_accuracy = sess.run(accuracy, feed_dict={x: X_train, y: Y_train})
+            validation_cost = sess.run(loss, feed_dict={x: X_valid, y: Y_valid})
+            validation_accuracy = sess.run(accuracy, feed_dict={x: X_valid, y: Y_valid})
+            print("After {} epochs".format(i))
+            print("\tTraining Cost: {}".format(train_cost))
+            print("\tTraining Accuracy: {}".format(train_accuracy))
+            print("\tValidation Cost: {}".format(validation_cost))
+            print("\tValidation Accuracy: {}".format(validation_accuracy))
+
+            if i < epochs:
+                X_S, Y_S = shuffle_data(X_train, Y_train)
+                for j in range(mbit):
+                    first = j * batch_size
+                    last = (j + 1) * batch_size
+                    last = X_train.shape[0] if last > X_train.shape[0] else last 
+                    ndict = {x: X_S[first:last], y: Y_S[first:last]}
+                    sess.run(train_op, feed_dict=ndict)
+                    if j != 0 and (j + 1) % 100 == 0:
+                        mcost = sess.run(loss, feed_dict=ndict)
+                        maccuracy = sess.run(accuracy, feed_dict=ndict) 
+                        print("\tStep {}:".format(j + 1))
+                        print("\t\tCost: {}".format(mcost))
+                        print("\t\tAccuracy: {}".format(maccuracy))
