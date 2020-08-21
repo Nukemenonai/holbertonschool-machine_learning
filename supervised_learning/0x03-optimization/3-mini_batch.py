7ec5782a5640561ec0e73895a8f2feacd8bda743
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-21 13:34:01 +0000

    savinf session

--- supervised_learning/0x03-optimization/3-mini_batch.py
+++ supervised_learning/0x03-optimization/3-mini_batch.py
@@ -25,26 +25,29 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
 
         for i in range(epochs + 1):
             train_cost = sess.run(loss, feed_dict={x: X_train, y: Y_train})
-            train_accuracy = sess.run(accuracy, feed_dict={x: X_train, y: Y_train})
-            validation_cost = sess.run(loss, feed_dict={x: X_valid, y: Y_valid})
-            validation_accuracy = sess.run(accuracy, feed_dict={x: X_valid, y: Y_valid})
+            train_acc = sess.run(accuracy, feed_dict={x: X_train, y: Y_train})
+            val_cost = sess.run(loss, feed_dict={x: X_valid, y: Y_valid})
+            val_acc = sess.run(accuracy, feed_dict={x: X_valid, y: Y_valid})
             print("After {} epochs".format(i))
             print("\tTraining Cost: {}".format(train_cost))
             print("\tTraining Accuracy: {}".format(train_accuracy))
-            print("\tValidation Cost: {}".format(validation_cost))
-            print("\tValidation Accuracy: {}".format(validation_accuracy))
+            print("\tValidation Cost: {}".format(val_cost))
+            print("\tValidation Accuracy: {}".format(val_accuracy))
 
             if i < epochs:
                 X_S, Y_S = shuffle_data(X_train, Y_train)
                 for j in range(mbit):
                     first = j * batch_size
                     last = (j + 1) * batch_size
-                    last = X_train.shape[0] if last > X_train.shape[0] else last 
+                    last = X_train.shape[0] if last > X_train.shape[0] else last
                     ndict = {x: X_S[first:last], y: Y_S[first:last]}
                     sess.run(train_op, feed_dict=ndict)
                     if j != 0 and (j + 1) % 100 == 0:
                         mcost = sess.run(loss, feed_dict=ndict)
-                        maccuracy = sess.run(accuracy, feed_dict=ndict) 
+                        maccuracy = sess.run(accuracy, feed_dict=ndict)
                         print("\tStep {}:".format(j + 1))
                         print("\t\tCost: {}".format(mcost))
                         print("\t\tAccuracy: {}".format(maccuracy))
+
+        saver.save(sess, save_path)
+    return save_path
