add138b043172ceb5e739762f02f4ec8c5253010
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-22 16:29:37 +0000

    fix: pycodestyle

--- supervised_learning/0x03-optimization/6-momentum.py
+++ supervised_learning/0x03-optimization/6-momentum.py
@@ -3,9 +3,10 @@
 
 import tensorflow as tf
 
+
 def create_momentum_op(loss, alpha, beta1):
     """ creates training operation in tensorflow
-    using GD with momentum 
+    using GD with momentum
     """
-    return tf.train.MomentumOptimizer(learning_rate=alpha, 
-                                        momentum=beta1).minimize(loss=loss)
+    return tf.train.MomentumOptimizer(learning_rate=alpha,
+                                      momentum=beta1).minimize(loss=loss)
