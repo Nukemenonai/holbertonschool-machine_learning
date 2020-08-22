4b12884eed1c7b0ae66d558914fdbe7725f5ecc2
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-22 16:27:30 +0000

    creates training operation in tensorflow using GD with momentum

---
+++ supervised_learning/0x03-optimization/6-momentum.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" contains the omentum upgraded function """
+
+import tensorflow as tf
+
+def create_momentum_op(loss, alpha, beta1):
+    """ creates training operation in tensorflow
+    using GD with momentum 
+    """
+    return tf.train.MomentumOptimizer(learning_rate=alpha, 
+                                        momentum=beta1).minimize(loss=loss)
