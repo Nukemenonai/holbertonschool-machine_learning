76f39ab9d871176b3d3728eba7939466860b06cd
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-22 21:25:14 +0000

    fix: pycodestyle

--- supervised_learning/0x03-optimization/7-RMSProp.py
+++ supervised_learning/0x03-optimization/7-RMSProp.py
@@ -3,15 +3,16 @@
 
 import numpy as np
 
+
 def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     """updates a variable using RMSProp optimization algo
     alpha: learning rate
-    beta2: RMSProp weight 
+    beta2: RMSProp weight
     epsilon: small number to avoid division by zero
     var: numpy.ndarray with variable to be updated
     grad: gradient of var (np.ndarray)
     s: previous second moment of var
-    Return: updated var  and new moment. 
+    Return: updated var  and new moment.
     """
     Sd = (beta2 * s) + ((1 - beta2) * (grad ** 2))
     var_updated = var - ((alpha * grad) / (np.sqrt(Sd) + epsilon))
