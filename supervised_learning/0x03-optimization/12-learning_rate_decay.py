a3241025e1981d6feb8b102ff400ae063fa0914c
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-23 01:45:52 +0000

    updates learning rate using inverse time decay

---
+++ supervised_learning/0x03-optimization/12-learning_rate_decay.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""learning rate decay module"""
+
+import tensorflow as tf
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """creates the learning rate decay operation using tensorflow"""
+    return tf.train.inverse_time_decay(alpha,
+                                       global_step,
+                                       decay_step,
+                                       decay_rate,
+                                       staircase = True)
