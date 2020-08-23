d06c37e42582f8450721882de0f5dea6e3934d05
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-23 01:34:59 +0000

    updates learning rate using inverse time decay

--- supervised_learning/0x03-optimization/11-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/11-learning_rate_decay.py
@@ -11,8 +11,6 @@ def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     decay_rate: weith used to determine rate at which alpha decays
     global_step: number of passes of GD elapsed
     decay_step: number of passes that should occur before alpha decays
-    Return: updated alpha 
+    Return: updated alpha
     """
     return alpha / (1 + decay_rate * np.floor(global_step / decay_step))
-
-
