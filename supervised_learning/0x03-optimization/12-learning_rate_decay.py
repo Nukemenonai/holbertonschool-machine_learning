8322ea7111b72b380ac62b12624bd974b4501ec8
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-23 01:47:21 +0000

    updates learning rate using inverse time decay

--- supervised_learning/0x03-optimization/12-learning_rate_decay.py
+++ supervised_learning/0x03-optimization/12-learning_rate_decay.py
@@ -10,4 +10,4 @@ def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
                                        global_step,
                                        decay_step,
                                        decay_rate,
-                                       staircase = True)
+                                       staircase=True)
