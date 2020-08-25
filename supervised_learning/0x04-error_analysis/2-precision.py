e246292199ca0b1f509b1fab94c63fae6d7a9477
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-25 00:48:31 +0000

    calculates precisionfor each class in a confusion matrix

---
+++ supervised_learning/0x04-error_analysis/2-precision.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""""""
+
+import numpy as np
+
+
+def precision(confusion):
+    """calcultes the precision for each class
+    in a confusion matrix"""
+    TP = np.diagonal(confusion)
+    FP_TP = np.sum(confusion, axis=0)
+    return TP / FP_TP
