9ceb93eae7e22d08a13d9bda70ecc44aec44dcfe
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-25 01:08:19 +0000

    calculates specificity of a confusion matrix

---
+++ supervised_learning/0x04-error_analysis/3-specificity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+def specificity(confusion):
+    """ calculates specificty for each class in a confussion matrix"""
+    TP = np.diagonal(confusion)
+    FN = np.sum(confusion, axis=1) - TP
+    FP = np.sum(confusion, axis=0) - TP
+    TN = np.sum(confusion) - (FP + FN + TP)
+    return TN / (TN + FP)
