92e0a5e3190b413962c807ca83b8ba0846f39201
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-25 01:26:55 +0000

    calculates f1 score of a confusion matrix

---
+++ supervised_learning/0x04-error_analysis/4-f1_score.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
+
+
+def f1_score(confusion):
+    """calculates the f1 score of a confusion matrix"""
+    TPR = sensitivity(confusion)
+    PPV = precision(confusion)
+    F1 = 2 * ((PPV * TPR)/(PPV + TPR))
+    return F1
