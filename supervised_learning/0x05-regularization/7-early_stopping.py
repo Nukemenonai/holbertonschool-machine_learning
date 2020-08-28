d542ad5e5d031cc1e28dee93280f088083c2a60b
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-28 02:33:33 +0000

    determines if a gd should be stopped

---
+++ supervised_learning/0x05-regularization/7-early_stopping.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""early stopping"""
+
+
+def early_stopping(cost, opt_cost, threshold, patience, count):
+    """destermines if GD should be stopped early"""
+
+    if opt_cost - cost > threshold:
+        count = 0
+    else:
+        count += 1
+    if count = patience:
+        return True, count
+    else:
+        return False, count
