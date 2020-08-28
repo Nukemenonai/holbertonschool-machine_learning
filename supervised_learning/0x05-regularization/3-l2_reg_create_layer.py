0313c602b39b3c88f6a363cae375b76a88c8b5b9
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-08-28 01:03:04 +0000

    creates a layer using l2 regularization

--- supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """l2_ reg create layer """
 
-import numpy as np
+import tensorflow as tf
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """creates a tensorflow layer that includes l2 regularization"""
 
     weights = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    reg = tf.contrib.layers.l2_regularizer(scale=lambtha)
+    reg = tf.contrib.layers.l2_regularizer(lambtha)
     return tf.layers.Dense(n, activation,
                            kernel_initializer=weights,
                            kernel_regularizer=reg).apply(prev)
