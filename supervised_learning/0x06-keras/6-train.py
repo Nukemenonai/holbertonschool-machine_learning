3c8132580987311c7b8e33d28108d5b6bf49f513
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-09-01 18:17:09 +0000

    trains using earrly stoping

--- supervised_learning/0x06-keras/6-train.py
+++ supervised_learning/0x06-keras/6-train.py
@@ -6,7 +6,7 @@ import tensorflow.keras as K
 
 
 def train_model(network, data, labels, batch_size, epochs, validation_data=None,
-                early_stopping=False, patience=0, verbose=True, shuffle=False)
+                early_stopping=False, patience=0, verbose=True, shuffle=False):
     """ trains a model using mini batch gradient descent
     validation_data is the data to validate the model with, if not None
     early_stopping boolean that indicates whether early stopping should be used
@@ -17,13 +17,13 @@ def train_model(network, data, labels, batch_size, epochs, validation_data=None,
 
     if validation_data:
         callbacks_list.append(K.callbacks.EarlyStopping(monitor='val_loss',
-                                                        patience=patience)
+                                                        patience=patience))
     history = network.fit(x=data,
                           y=labels,
                           batch_size=batch_size,
                           epochs=epochs,
                           verbose=verbose,
                           validation_data=validation_data,
-                          shuffle=shuffle
+                          shuffle=shuffle,
                           callbacks=callbacks_list)
     return history
