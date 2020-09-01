97fce2706413a0e2c8569fd8536731419b6ad5a4
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-09-01 17:36:09 +0000

    trains model also using validation data

--- supervised_learning/0x06-keras/5-train.py
+++ supervised_learning/0x06-keras/5-train.py
@@ -9,5 +9,7 @@ def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, verbose=True, shuffle=False):
     """ trains a model using mini batch gradient descent"""
     return network.fit(x=data, y=labels, batch_size=batch_size,
-                          epochs=epochs, validation_data=validation_data,
-                          verbose=verbose, shuffle=shuffle)
+                       epochs=epochs,
+                       verbose=verbose,
+                       validation_data=validation_data,
+                       shuffle=shuffle)
