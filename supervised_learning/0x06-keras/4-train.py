441f2f8ca9d64c1eb868dd0f433b68d691758a6c
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-09-01 17:29:39 +0000

    trains using mini batch gradient descent

--- supervised_learning/0x06-keras/4-train.py
+++ supervised_learning/0x06-keras/4-train.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 """trais a model using mini batch gradient descent"""
 
+
+import tensorflow.keras as K
+
+
 def train_model(network, data, labels, batch_size, epochs,
                 verbose=True, shuffle=False):
     """ trains a model using mini batch gradient descent"""
