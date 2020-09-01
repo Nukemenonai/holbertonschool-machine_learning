9fa4ce91581e60675c8f8707df13dc3e33a51686
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-09-01 13:19:14 +0000

    adam optimization with categorical crossentropy

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -7,8 +7,8 @@ import tensorflow.keras as K
 def optimize_model(network, alpha, beta1, beta2):
     """Adam optimization for a keras model with
     categorical crossentropy loss and accuracy metrics:"""
-    Adam = K.optimizers.Adam(lr=alpha,beta_1=beta1, beta_2=beta2)
-    network.compile(loss='categorical_crossentropy',
+    Adam = K.optimizers.Adam(lr=alpha, beta_1=beta1, beta_2=beta2)
+    network.compile(loss=K.losses.CategoricalCrossentropy(),
                     optimizer=Adam,
                     metrics=['accuracy'])
     return None
