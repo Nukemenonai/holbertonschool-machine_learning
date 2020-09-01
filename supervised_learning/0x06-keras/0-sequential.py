55eed14b6d6a400aa773697f730ace4ce7ad183a
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-09-01 01:38:01 +0000

    model

---
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" build_model module """
+
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    """builds a neural network with the kearas library"""
+    L2 = K.regularizers.l2(lambtha)
+    model = K.Sequential()
+    for i in range(len(layers)):
+        model.add(K.layers.Dense(units=layers[i],
+                                 activation=activations[i],
+                                 kernel_regularizer=L2, input_shape=(nx,)))
+        if i != len(layers) - 1:
+            model.add(K.layers.Dropout(keep_prob))
+    return model
