b214c3124f79a76229487e14911accb1687e18cd
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-09-04 02:38:02 +0000

    valid convolution with same

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -12,7 +12,7 @@ def convolve_grayscale_same(images, kernel):
     pad_w = int(kw / 2) if kw % 2 == 0 else int((kw - 1) / 2)
     pad_h = int(kh / 2) if kh % 2 == 0 else int((kh - 1) / 2)
 
-    padded = np.pad(images, (0, 0), (pad_h, pad_h), (pad_w, pad_w), 'constant')
+    padded = np.pad(images, ((0, 0), (pad_h, pad_h), (pad_w, pad_w)), 'constant')
     m, h, w = images.shape
     conv = np.zeros((m, h, w))
     for i in range(h):
