5d81eac721f276e0409d4f36a3912b59669f1676
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-09-04 18:14:27 +0000

    convolving with stride

--- math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -11,14 +11,13 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     m, h, w = images.shape
     kh, kw = kernel.shape
     sh, sw = stride
-
+    pad_h, pad_w = (0, 0)
     if padding == 'same':
         pad_w = int((((iw - 1) * sw + kw - w) / 2) + 1)
         pad_h = int((((ih - 1) * sh + kh - w) / 2) + 1)
     elif type(padding) == tuple:
         pad_h, pad_w = padding
-    else:
-        pad_h, pad_w = (0, 0)
+
     padded = np.pad(images, ((0, 0), (pad_h, pad_h),
                              (pad_w, pad_w)), 'constant')
     output_h = (((h + (pad_h * 2) - kh) // sh) + 1)
@@ -26,8 +25,8 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     conv = np.zeros((m, output_h, output_w))
     for i in range(output_h):
         for j in range(output_w):
-            sector = padded[:, (i * sh):(i * sh) + kh,
-                            (j * sw):(j * sw) + kw]
+            sector = padded[:, (i * sh):(sh * i) + kh,
+                            (j * sw):(sw * j) + kw]
             s_c = sector * kernel
             p = np.sum(s_c, axis=1)
             p2 = np.sum(p, axis=1)
