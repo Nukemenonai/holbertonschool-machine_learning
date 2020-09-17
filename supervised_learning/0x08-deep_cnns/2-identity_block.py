1d2942ed27513e5151524fe965edf97bf63b09e2
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-09-17 20:10:49 +0000

    builds an identity block with keras

--- supervised_learning/0x08-deep_cnns/2-identity_block.py
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -11,10 +11,11 @@ def identity_block(A_prev, filters):
     A_prev: output from prevoius layer
     filters: number of filters in each convolution"""
     F11, F3, F12 = filters
-
+    initializer = K.initializers.he_normal()
     conv_F11 = K.layers.Conv2D(filters=F11,
                                kernel_size=(1, 1),
-                               padding='same')
+                               padding='same',
+                               kernel_initializer=initializer)
     conv_F11 = conv_F11(A_prev)
 
     norm1 = K.layers.BatchNormalization(axis=3)
@@ -24,7 +25,8 @@ def identity_block(A_prev, filters):
 
     conv_F3 = K.layers.Conv2D(filters=F3,
                               kernel_size=(3, 3),
-                              padding='same')
+                              padding='same',
+                              kernel_initializer=initializer)
     conv_F3 = conv_F3(X1)
 
     norm2 = K.layers.BatchNormalization(axis=3)
@@ -34,7 +36,8 @@ def identity_block(A_prev, filters):
 
     conv_F12 = K.layers.Conv2D(filters=F12,
                                kernel_size=(1, 1),
-                               padding='same')
+                               padding='same',
+                               kernel_initializer=initializer)
     conv_F12 = conv_F12(X2)
 
     norm3 = K.layers.BatchNormalization(axis=3)
