42b44adb9bff524cb4db11e81b5492c2c941695f
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-09-17 18:41:40 +0000

    builds an identity block with keras

---
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+identity block
+"""
+
+import tensorflow.keras as K
+
+
+def identity_block(A_prev, filters):
+    """builds an identity block
+    A_prev: output from prevoius layer
+    filters: number of filters in each convolution"""
+    F11, F3, F12 = filters
+
+    conv_F11 = K.layers.Conv2D(filters=F11,
+                               kernel_size=(1, 1),
+                               padding='same')
+    conv_F11 = conv_F11(A_prev)
+
+    norm1 = K.layers.BatchNormalization(axis=3)
+    norm1 = norm1(conv_F11)
+
+    X1 = K.layers.Activation('relu')(norm1)
+
+    conv_F3 = K.layers.Conv2D(filters=F3,
+                              kernel_size=(3, 3),
+                              padding='same')
+    conv_F3 = conv_F3(X1)
+
+    norm2 = K.layers.BatchNormalization(axis=3)
+    norm2 = norm2(conv_F3)
+
+    X2 = K.layers.Activation('relu')(norm2)
+
+    conv_F12 = K.layers.Conv2D(filters=F12,
+                               kernel_size=(1, 1),
+                               padding='same')
+    conv_F12 = conv_F12(X2)
+
+    norm3 = K.layers.BatchNormalization(axis=3)
+    norm3 = norm3(conv_F12)
+
+    add = K.layers.Add()([norm3, A_prev])
+    X3 = K.layers.Activation('relu')(add)
+
+    return X3
