e7bbee5e0defd51281e6c47d9d3c6fb098a43a0d
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-09-17 12:35:10 +0000

    builds an inception network as depicted in 'going deeper with convolutions' with keras

--- supervised_learning/0x08-deep_cnns/1-inception_network.py
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -8,27 +8,27 @@ inception_block = __import__('0-inception_block').inception_block
 
 def inception_network():
     """ builds an inception network"""
-    input = K.input(shape=(224,224,3))
-    conv1 = K.layers.Conv2(filters=64, kernel_size=(7, 7),
+    input = K.Input(shape=(224,224,3))
+    conv1 = K.layers.Conv2D(filters=64, kernel_size=(7, 7),
                            strides=(2, 2), padding='same',
                            activation='relu')
     conv1 = conv1(input)
 
     maxpool1 = K.layers.MaxPooling2D((3,3), strides=(2, 2), padding='same')
-    maxpool1 = maxpool(conv1)
+    maxpool1 = maxpool1(conv1)
 
-    conv2 = K.layers.Conv2(filters=64, kernel_size=(1, 1),
+    conv2 = K.layers.Conv2D(filters=64, kernel_size=(1, 1),
                            padding='same',
                            activation='relu')
     conv2 = conv2(maxpool1)
 
-    conv3 = K.layers.Conv2(filters=192, kernel_size=(3, 3),
+    conv3 = K.layers.Conv2D(filters=192, kernel_size=(3, 3),
                            padding='same',
                            activation='relu')
     conv3 = conv3(conv2)
 
     maxpool2 = K.layers.MaxPooling2D((3,3), strides=(2, 2), padding='same')
-    maxpool2 = maxpool(conv3)
+    maxpool2 = maxpool2(conv3)
 
     inception1 = inception_block(maxpool2, [64,96, 128, 16, 32, 32])
     inception2 = inception_block(inception1, [128, 128, 192, 32, 96, 64])
@@ -45,5 +45,17 @@ def inception_network():
     maxpool4 = K.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')
     maxpool4 = maxpool4(inception7)
 
-    inception8 = inception_block(maxpool5, [256, 160, 320, 32, 128, 128])
-    inception9 = inception_block(inception6, [256, 160, 320, 32, 128, 128])
+    inception8 = inception_block(maxpool4, [256, 160, 320, 32, 128, 128])
+    inception9 = inception_block(inception8, [384, 192, 384, 48, 128, 128])
+
+    avg_pool = K.layers.AveragePooling2D((7, 7), strides=(1, 1))
+    avg_pool = avg_pool(inception9)
+
+    dropout = K.layers.Dropout(0.4)
+    dropout = dropout(avg_pool)
+
+    FC1 = K.layers.Dense(1000, activation='softmax')
+    FC1 = FC1(dropout)
+
+    model = K.models.Model(inputs=input, outputs=FC1)
+    return model
