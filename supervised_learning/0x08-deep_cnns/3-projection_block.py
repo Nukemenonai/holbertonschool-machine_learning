1e5eccae8fa9bb6b558cb4fcf50e1ca166a5ea0d
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-09-18 00:37:21 +0000

    builds an projection block with keras

---
+++ supervised_learning/0x08-deep_cnns/3-projection_block.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+identity block
+"""
+
+import tensorflow.keras as K
+
+
+def projection_block(A_prev, filters, s=2):
+    """builds an projection block
+    A_prev: output from prevoius layer
+    filters: number of filters in each convolution
+    s: stride of the first convolution in both main path
+    and shortcut connection
+    """
+    F11, F3, F12 = filters
+    initializer = K.initializers.he_normal()
+    conv_F11 = K.layers.Conv2D(filters=F11,
+                               kernel_size=(1, 1),
+                               padding='same',
+                               strides=(s, s),
+                               kernel_initializer=initializer)
+    conv_F11 = conv_F11(A_prev)
+
+    norm1 = K.layers.BatchNormalization(axis=3)
+    norm1 = norm1(conv_F11)
+
+    X1 = K.layers.Activation('relu')(norm1)
+
+    conv_F3 = K.layers.Conv2D(filters=F3,
+                              kernel_size=(3, 3),
+                              padding='same',
+                              kernel_initializer=initializer)
+    conv_F3 = conv_F3(X1)
+
+    norm2 = K.layers.BatchNormalization(axis=3)
+    norm2 = norm2(conv_F3)
+
+    X2 = K.layers.Activation('relu')(norm2)
+
+    conv_F12 = K.layers.Conv2D(filters=F12,
+                               kernel_size=(1, 1),
+                               padding='same',
+                               kernel_initializer=initializer)
+    conv_F12 = conv_F12(X2)
+
+    norm3 = K.layers.BatchNormalization(axis=3)
+    norm3 = norm3(conv_F12)
+
+    shortcut = K.layers.Conv2D(filters=F12,
+                               kernel_size=(1, 1),
+                               padding='same',
+                               strides=(s, s))
+
+    add = K.layers.Add()([norm3, A_prev])
+    X3 = K.layers.Activation('relu')(add)
+
+    return X3
