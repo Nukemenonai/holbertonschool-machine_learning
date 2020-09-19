7d9890440cdb021ccbd36b1aba3a024f6a41ce73
Nukemenonai <david.giovanni.ovalle@gmail.com>
2020-09-19 02:13:21 +0000

    builds a transition layer in tensorflow keras

--- supervised_learning/0x08-deep_cnns/6-transition_layer.py
+++ supervised_learning/0x08-deep_cnns/6-transition_layer.py
@@ -12,12 +12,13 @@ def transition_layer(X, nb_filters, compression):
     compression: compression factor of transition layer
     """
     fltr = int(nb_filters * compression)
+    intializer = K.initializers.he_normal()
     norm1 = K.layers.BatchNormalization(axis=3)(X)
-    A = K.layers.BatchNormalization('relu')(norm1)
-    conv = K.layers.Conv2D(filters=filter,
-                           kernel_size(1, 1),
+    A = K.layers.Activation('relu')(norm1)
+    conv = K.layers.Conv2D(filters=fltr,
+                           kernel_size=(1, 1),
                            padding='same',
                            strides=(1, 1))(A)
-    avgpool = K.layers.AeragePooling2D(pool_size=(2, 2),
+    avgpool = K.layers.AveragePooling2D(pool_size=(2, 2),
                                        strides=(2, 2))(conv)
     return avgpool, fltr
