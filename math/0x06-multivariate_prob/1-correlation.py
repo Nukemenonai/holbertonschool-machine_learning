a208851722efd42c78ef0f98981ca5e112b451a0
David Ovalle Ariza <david.giovanni.ovalle@gmail.com>
2021-03-22 20:41:44 -0500

    fix: removed bad comments

--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -16,8 +16,6 @@ def correlation(C):
     if len(C.shape) is not 2 or C.shape[0] is not C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
 
-    # extract the variances from the diagonal elements
     D = np.sqrt(np.diag(C))
-    # get outer product of diagonal 
     outer = np.outer(D, D)
     return C / outer
