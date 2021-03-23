14011917ae07c74038700a9ddd564273be97ec87
David Ovalle Ariza <david.giovanni.ovalle@gmail.com>
2021-03-22 22:14:57 -0500

    damn shenanigans

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -36,3 +36,23 @@ class MultiNormal ():
         if n < 2:
             raise ValueError('data must contain multiple data points')
         self.mean, self.cov = mean_cov(data)
+
+    def pdf(self, x):
+        """ calculates the PDF at a data point
+        :param x: np.ndarray contains the data set
+            d number of dimensions of the multinomial instance
+        :return: the value of the PDF
+        """
+
+        if type(x) != np.ndarray:
+            raise TypeError("x must be a numpy.ndarray")
+        d = self.cov.shape[0]
+        if len(x.shape) != 2 or x.shape[0] != d or x.shape[1] != 1:
+            raise ValueError(f"x must have the shape ({d}, 1 )")
+
+        x_1 = x - self.mean
+
+        den = np.sqrt((2 * np.pi) ** d * np.linalg.det(self.cov))
+        exp = np.exp(-(np.linalg.solve(self.cov, x_1).T.dot(x_1)) / 2)
+        res = (1/den) * exp
+        return res[0][0]
