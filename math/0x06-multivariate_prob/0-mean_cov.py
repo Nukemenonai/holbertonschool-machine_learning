f21c0a3cd6626efd374d3d765540b57a75126cf7
David Ovalle Ariza <david.giovanni.ovalle@gmail.com>
2021-03-22 19:52:22 -0500

    fix: pycode style and bad return

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -10,12 +10,12 @@ def mean_cov(X):
         raise TypeError("X must be a 2D numpy.ndarray")
     n, d = X.shape
     if n < 2:
-        raise ValueError("X must cotain multiple data points")
+        raise ValueError("X must contain multiple data points")
     
     mean = np.expand_dims(np.mean(X, axis=0), axis=0)
     # taking the differences between each number in the dataset and the mean
-    X -= mean 
-    
+    X -= mean
+
     cov = np.dot(X.T, X) / (n - 1)
 
     return mean, cov
