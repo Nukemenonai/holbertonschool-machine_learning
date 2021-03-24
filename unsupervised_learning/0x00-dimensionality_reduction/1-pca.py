3d5f996d39bb82a132054de2a879f86fa70b3a67
Nukemenonai <david.giovanni.ovalle@gmail.com>
2021-03-24 07:43:51 -0500

    pca v2

---
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" PCA with X transformation """
+
+import numpy as np
+
+def pca(X, ndim):
+    """
+    Performs PCA on a dataset
+    """
+    X_mean = X - np.mean(X, axis=0, keepdims=True)
+    U, S, Vh = np.linalg.svd(X_mean)
+    
+    W = Vh.T
+    Wr = W[:, :ndim]
+
+    Tr = np.dot(X_mean, Wr)
+    return Tr 
