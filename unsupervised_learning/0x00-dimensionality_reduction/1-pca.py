68aa454f91a46e9e488a18a239b9f35615be4d5b
Nukemenonai <david.giovanni.ovalle@gmail.com>
2021-03-24 19:36:27 -0500

    fix: pycodestyle

--- unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -3,15 +3,16 @@
 
 import numpy as np
 
+
 def pca(X, ndim):
     """
     Performs PCA on a dataset
     """
     X_mean = X - np.mean(X, axis=0, keepdims=True)
     U, S, Vh = np.linalg.svd(X_mean)
-    
+
     W = Vh.T
     Wr = W[:, :ndim]
 
     Tr = np.dot(X_mean, Wr)
-    return Tr 
+    return Tr
