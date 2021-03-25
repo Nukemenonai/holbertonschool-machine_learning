ed3d66ed689eafbf64ff560274381de91c391d80
Nukemenonai <david.giovanni.ovalle@gmail.com>
2021-03-24 19:26:11 -0500

    fix: pycodestyle

--- unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
@@ -5,28 +5,27 @@ import numpy as np
 
 
 def pca(X, var=0.95):
-    """ 
+    """
     performs PCA on a dataset
-    X: numpy.ndarray 
+    X: numpy.ndarray
         n is the number of data points
         d number of dimensions in each point
-    var: fraction of the variance that PCA should maintain 
-    returns: the weight matrix 
+    var: fraction of the variance that PCA should maintain
+    returns: the weight matrix
     """
 
-    # singulr value decomposition 
+    # singulr value decomposition
     U, S, Vh = np.linalg.svd(X)
 
-    # get the threshold by multipling the last item 
+    # get the threshold by multipling the last item
     # of the cumulative sum of singular values by the variance fragment
     c_s_arr = np.cumsum(S)
     threshold = c_s_arr[-1] * var
 
-    mask = np.where(c_s_arr < threshold) 
+    mask = np.where(c_s_arr < threshold)
     r = len(c_s_arr[mask])
 
     W = Vh.T
-    # truncate to the first r values  
+    # truncate to the first r values
     Wr = W[:, :r+1]
-    return Wr 
-
+    return Wr
