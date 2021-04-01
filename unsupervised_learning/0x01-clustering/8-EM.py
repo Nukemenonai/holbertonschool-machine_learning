261efc6aafe90aad59268a9c276431540f972347
Nukemenonai <david.giovanni.ovalle@gmail.com>
2021-03-31 22:50:34 -0500

    fix: update in loop events order

--- unsupervised_learning/0x01-clustering/8-EM.py
+++ unsupervised_learning/0x01-clustering/8-EM.py
@@ -19,35 +19,45 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
     tol is a non-negative float containing tolerance
     of the log likelihood, used to determine early stopping
     """
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-        return None, None, None, None, None
-    if type(k) != int or k <= 0:
-        return None, None, None, None, None
-    if type(iterations) != int or iterations <= 0:
-        return None, None, None, None, None
-    if type(tol) != float or tol < 0:
-        return None, None, None, None, None
-    if type(verbose) != bool:
-        return None, None, None, None, None
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return (None, None, None, None, None)
+
+    if type(k) is not int or k <= 0:
+        return (None, None, None, None, None)
+
+    if type(iterations)is not int or iterations <= 0:
+        return (None, None, None, None, None)
+
+    if type(tol) is not float or tol < 0:
+        return (None, None, None, None, None)
+
+    if type(verbose) is not bool:
+        return (None, None, None, None, None)
 
     pi, m, S = initialize(X, k)
-    # L = likelihood
     prev_L = 0
+    i = 0
 
-    for i in range(iterations):
-        g, L = expectation(X, pi, m, S)
-        pi, m, S = maximization(X, g)
-
-        if verbose:
-            message = 'Log Likelihood after {} iterations: {}'\
-                .format(i, L.round(5))
-            if (i % 10 == 0) or (i == 0):
-                print(message)
-
-            if abs(L - prev_L) <= tol:
-                print(message)
-                break
-        if abs(L - prev_L) <= tol:
+     # L = likelihood
+    g, L = expectation(X, pi, m, S)
+    while(i < iterations):
+        if (np.abs(prev_L - L)) <= tol:
             break
+
+        if verbose is True and i % 10 == 0:
+            m1 = 'Log Likelihood after {}'.format(i)
+            m2 = ' iterations: {}'.format(L.round(5))
+            print(m1 + m2)
+
         prev_L = L
-    return pi, m, S, g, L
+        pi, m, S = maximization(X, g)
+        g, L = expectation(X, pi, m, S)
+
+        i += 1
+
+    if verbose is True:
+        msg = 'Log Likelihood after {} iterations: {}'\
+            .format(i, L.round(5))
+        print(msg)
+
+    return (pi, m, S, g, L)
