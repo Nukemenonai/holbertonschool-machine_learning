1649ff190e5b62675e13589fc61fd3ce39aacec2
Nukemenonai <david.giovanni.ovalle@gmail.com>
2021-03-31 23:32:58 -0500

    update: module doc

--- unsupervised_learning/0x01-clustering/8-EM.py
+++ unsupervised_learning/0x01-clustering/8-EM.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-EM
+Expectation Maximization 
 """
 
 import numpy as np
@@ -11,7 +11,6 @@ maximization = __import__('7-maximization').maximization
 
 def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
     """performs the expectation maximization for a GMM:
-
     X: (n, d) the data set
     k: positive integer, the number of clusters
     iterations: positive integer,the maximum number
