ea408a6c0d5cb7808e246f3584ca3da8c70d5db4
Nukemenonai <david.giovanni.ovalle@gmail.com>
2021-03-31 23:06:17 -0500

    kmeans with sklearn

---
+++ unsupervised_learning/0x01-clustering/10-kmeans.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import sklearn.cluster
+
+
+def kmeans(X, k):
+
+    """performs k mmeans on a data set """
+    kmeans = sklearn.cluster.KMeans(n_clusters=k).fit(X)
+
+    C = kmeans.cluster_centers_
+    clss = kmeans.labels_
+
+    return C, clss
