ae6372f1750fd7a3884a87a75db5981041ef0423
Nukemenonai <david.giovanni.ovalle@gmail.com>
2021-04-29 08:46:59 -0500

    single GRU cell

---
+++ supervised_learning/0x0D-RNNs/2-gru_cell.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""
+creating single gated recurring unit cell
+"""
+
+import numpy as np
+
+
+class GRUCell():
+    """
+    Represents a gated recurrent unit
+    """
+    def __init__(self, i, h, o):
+        """
+        * i:  dimensionality of the data
+        * h:  dimensionality of the hidden state
+        * o:  dimensionality of the outputs
+        * Wz and bz are for the update gate
+        * Wr and br are for the reset gate
+        * Wh and bh are for the intermediate hidden state
+        * Wy and by are for the output
+        * The weights should be initialized using a random normal
+          distribution in the order listed above
+        * The weights will be used on the right side for matrix
+          multiplication
+        * The biases should be initialized as zero
+        """
+        self.Wz = np.random.normal(size=(i + h, h))
+        self.Wr = np.random.normal(size=(i + h, h))
+        self.Wh = np.random.normal(size=(i + h, h))
+        self.Wy = np.random.normal(size=(h, o))
+
+        self.bz = np.zeros((1, h))
+        self.br = np.zeros((1, h))
+        self.bh = np.zeros((1, h))
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """
+        * x_t:  numpy.ndarray of shape (m, i) that contains the data
+          input for the cell
+        * m:  batche size for the data
+        * h_prev:  numpy.ndarray of shape (m, h) containing the
+          previous hidden state
+        Returns: h_next, y
+        * h_next:  next hidden state
+        * y:  output of the cell
+        """
+        xh = np.concatenate((h_prev, x_t), axis=1)
+
+        # rt = reset gate
+        rt = np.dot(xh, self.Wr) + self.br
+        rt = 1 / (1 + np.exp(-rt))
+
+        # zt = update gate
+        zt = np.dot(xh, self.Wz) + self.bz
+        zt = 1 / (1 + np.exp(-zt))
+
+        # h_hat = current state
+        r_h = rt * h_prev
+        r_hx = np.concatenate((r_h, x_t), axis=1)
+        h_hat = np.tanh(np.dot(r_hx, self.Wh) + self.bh)
+
+        # h_next
+        h_next = zt * h_hat + (1 - zt) * h_prev
+
+        # output GRU cell
+        y_pred = np.dot(h_next, self.Wy) + self.by
+        y = np.exp(y_pred) / np.sum(np.exp(y_pred), axis=1, keepdims=True)
+
+        return (h_next, y)
