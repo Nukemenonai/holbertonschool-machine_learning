61d1565486a0fffddd7c9e7df0ca86cfc7b316b8
Nukemenonai <david.giovanni.ovalle@gmail.com>
2021-06-22 12:00:29 -0500

    load frozenlake environment

---
+++ reinforcement_learning/0x00-q_learning/0-load_env.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Load Environment    
+"""
+
+import numpy as np
+import gym
+
+def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
+    """
+    Loads the pre-made FrozenLakeEnv evnironment from OpenAI’s gym:
+
+    -desc: either None or a list of lists containing 
+    a custom description of the map to load for the environment
+    -map_name: either None or a string containing 
+    the pre-made map to load
+    Note: If both desc and map_name are None, 
+    the environment will load a randomly generated 8x8 map
+    is_slippery: boolean to determine if the ice is slippery
+    Returns: the environment
+    """
+
+    env = gym.make(id='FrozenLake-v0', desc=desc,
+                           map_name=map_name, is_slippery=is_slippery)
+
+    return env
