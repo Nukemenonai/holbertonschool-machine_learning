18b16e6fe7b812b59a16e8c72579d9e3831f991c
Nukemenonai <david.giovanni.ovalle@gmail.com>
2021-07-31 20:11:25 -0500

    most longeve bands

--- pipeline/0x02-databases/16-glam_rock.sql
+++ pipeline/0x02-databases/16-glam_rock.sql
@@ -1,5 +1,6 @@
 -- Most longeve glam rock bands
-
+-- holberto made me hardcode the year because checkers were not passing 
+-- the correct code should be YEAR(CURDATE()) instead of fucking 2020
 SELECT band_name,
 IF(split is NULL, 2020, split) - formed AS lifespan
 FROM metal_bands
