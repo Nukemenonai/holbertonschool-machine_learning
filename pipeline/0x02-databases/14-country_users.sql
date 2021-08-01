848200c2fca0143394a9a56a53286127435d9802
Nukemenonai <david.giovanni.ovalle@gmail.com>
2021-07-31 19:46:02 -0500

    in and not out

---
+++ pipeline/0x02-databases/14-country_users.sql
@@ -0,0 +1,10 @@
+-- new table with not nullable fields
+CREATE TABLE IF NOT EXISTS users
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL,
+    PRIMARY KEY (id)
+);
+--
