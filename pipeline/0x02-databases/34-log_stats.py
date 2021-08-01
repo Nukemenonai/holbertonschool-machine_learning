a8db9cfe4155fbcba071817781710d1c4046f045
Nukemenonai <david.giovanni.ovalle@gmail.com>
2021-08-01 18:14:54 -0500

    log stats

--- pipeline/0x02-databases/34-log_stats.py
+++ pipeline/0x02-databases/34-log_stats.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python3
 """
-Log stats using pymongo
+Log stats w/ pymongo
 """
 
 from pymongo import MongoClient
 
 
 if __name__ == "__main__":
+    """
+    * Database: logs
+    * Collection: nginx
+    * Display (same as the example):
+      - first line: x logs where x is the number of
+        documents in this collection
+      - second line: Methods:
+          - 5 lines with the number of documents with the
+            method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+            in this order (see example below - warning: it’s a
+            tabulation before each line)
+          - one line with the number of documents with:
+            method=GET
+            path=/status
+    """
 
     client = MongoClient('mongodb://127.0.0.1:27017')
 
@@ -23,7 +38,7 @@ if __name__ == "__main__":
         num_met = collection_logs.count_documents({"method": method})
         print("\tmethod {}: {}".format(method, num_met))
 
-    my_dict = {"method": "GET", "path": "/status"}
+    dictionary = {"method": "GET", "path": "/status"}
 
-    num_dict = collection_logs.count_documents(my_dict)
-    print("{} status check".format(num_dict))
+    n_dict = collection_logs.count_documents(dictionary)
+    print("{} status check".format(n_dict))
