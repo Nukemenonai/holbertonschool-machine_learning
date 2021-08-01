d10dcddca8f89476dea6e166fba9a38ee554d860
Nukemenonai <david.giovanni.ovalle@gmail.com>
2021-08-01 08:37:41 -0500

    safely divides two integers

--- pipeline/0x02-databases/21-div.sql
+++ pipeline/0x02-databases/21-div.sql
@@ -3,16 +3,19 @@
 DELIMITER $$
 
 CREATE FUNCTION SafeDiv(a INT, b INT)
+
 RETURNS FLOAT
+
 BEGIN
-    DECLARE result FLOAT;
-    SET result = 0.0;
-    IF b = 0 THEN
-        SET result = 0;
-    ELSE
-        SET result a / b;
-    END IF;
-    RETURN result;
+	DECLARE result FLOAT;
+	SET result = 0.0;
+	IF b = 0 THEN
+	   SET result = 0;
+	ELSE
+	   SET result = a / b;
+	END IF;
+	
+	RETURN result;
 END $$
 
-DELIMITER ; 
+DELIMITER ;
