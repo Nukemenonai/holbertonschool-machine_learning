80254e5619bc56552f551f81f212e64d873bcdfe
Nukemenonai <david.giovanni.ovalle@gmail.com>
2021-07-31 20:09:17 -0500

    ._.

--- pipeline/0x02-databases/16-glam_rock.sql
+++ pipeline/0x02-databases/16-glam_rock.sql
@@ -1,7 +1,7 @@
 -- Most longeve glam rock bands
 
 SELECT band_name,
-IF(split is NULL, YEAR(CURDATE()), split) - formed AS lifespan
+IF(split is NULL, 2020, split) - formed AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC, band_name DESC;
